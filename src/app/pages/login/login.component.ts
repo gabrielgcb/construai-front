d2ca73f50b2ef06beca204ffba9aa02ea0881bf8
Gabriel Bispo <gabriel.bispo@discente.ufg.br>
2024-06-22 18:31:22 -0300

    feat: layout inicial do login

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { ContainerComponent } from '../../componentes/container/container.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    ContainerComponent,
+    ReactiveFormsModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  loginForm!: FormGroup;
+
+  constructor() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    })
+  }
+}
