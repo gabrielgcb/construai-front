c652c2b2e10a04474cfde4b28eadd6ab8f8c4f02
Gabriel Bispo <gabriel.bispo@discente.ufg.br>
2024-06-22 22:42:11 -0300

    feat: adicionando validações na tela de login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,9 @@
+import { LoginService } from './../../services/login.service';
 import { Component } from '@angular/core';
 import { ContainerComponent } from '../../componentes/container/container.component';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -9,17 +12,30 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
     ContainerComponent,
     ReactiveFormsModule
   ],
+  providers: [
+    LoginService
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   loginForm!: FormGroup;
 
-  constructor() {
+  constructor(
+    private router: Router,
+    private loginService: LoginService,
+    private toastrService: ToastrService
+  ) {
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)])
     })
   }
 
+  submit() {
+    this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+      next: () => this.toastrService.success("Login feito com sucesso!"),
+      error: () => this.toastrService.error("Erro inesperado. Tente novamente mais tarde..."), 
+    }) 
+  }
 }
